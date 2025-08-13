3dc99165b02f985197043ed34b3d0e61fa255b24
Rizki Januar I. <rizki.20046@mhs.unesa.ac.id>
2025-08-13 15:46:44 +0700

    ✨ feat : .

--- config/filament-local-logins.php
+++ config/filament-local-logins.php
@@ -5,7 +5,8 @@ use BetterFuturesStudio\FilamentLocalLogins\Filament\Pages\Auth\LoginPage;
 return [
     'panels' => [
         'admin' => [
-            'enabled' => env('ADMIN_PANEL_LOCAL_LOGINS_ENABLED', true),
+            'enabled' => env('ADMIN_PANEL_LOCAL_LOGINS_ENABLED',
+                env('APP_ENV') === 'local' || env('APP_ENV') === 'production'),
             'emails' => array_filter(array_map('trim', explode(',', env('ADMIN_PANEL_LOCAL_LOGIN_EMAILS', '')))),
             'login_page' => LoginPage::class,
         ],
